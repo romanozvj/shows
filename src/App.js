f8421d89508a21dbdee71d10958d8772245ce330
Romano Zvjerković <romanozvj@gmail.com>
2018-07-10 11:10:07 +0200

    Added button that fetches and shows a list of shows

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor(args) {
+    super(args);
+
+    this.state = {
+      shows: [],
+    };
+    this._onButtonClick = this._onButtonClick.bind(this);
+  }
+
+  _onButtonClick() {
+    fetch('https://api.infinum.academy/api/shows')
+    //.then ((data) => console.log(data))
+      .then((data) => data.json())
+      .then((response) => this.setState({ shows: response.data }));
+  }
+
   render() {
+    const dataList = this.state.shows.map((show) => <li>{show.title}</li>);
     return (
-      <h1>
-        Hello Academy!
-      </h1>
+      <div>
+        {
+          this.state.shows.length > 0
+            ? <ul>{dataList}</ul>
+            : <button onClick={this._onButtonClick} type="button">
+                Load shows
+              </button>
+        }
+      </div>
     );
   }
 }
